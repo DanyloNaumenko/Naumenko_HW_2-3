94fca4362573dc4b667144265e918405612ef3ce
Данил Науменко <d.naumenko1111@gmail.com>
2024-05-02 18:27:52 +0200

    NaumenkoDanylo. Added class User

---
+++ Naumenko_HW_2-3/Gender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naumenko_HW_2_3
+{
+    internal enum Gender
+    {
+        Unknown = 0,
+        Male = 1,
+        Female = -1
+    }
+}
---
+++ Naumenko_HW_2-3/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naumenko_HW_2_3
+{
+    internal class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; }
+        public Gender Gender { get; set; }
+
+        public User() { }
+        public User(string age)
+        {
+            Age = age;
+        }
+    }
+}
